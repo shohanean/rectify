fe10d6ff165661c386353da8359b7cd1ad169f8b
Rayan Hossain <hossain_shohan1@live.com>
2023-07-15 20:02:35 +0600

    object used for form submit

--- src/pages/AddProduct.jsx
+++ src/pages/AddProduct.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 
 const AddProduct = () => {
-  const [productName, setProductName] = useState("");
-  const [productPrice, setProductPrice] = useState("");
+  const [inputs, setInputs] = useState({
+    productName: "",
+    productPrice: "",
+  });
+  const handleChange = (event) => {
+    setInputs({
+      ...inputs,
+      [event.target.name]: event.target.value,
+    });
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert("Product Name: " + productName);
-    alert("Product Name: " + productPrice);
-    setProductName("");
-    setProductPrice("");
+    alert("Product Name: " + inputs.productName);
+    alert("Product Price: " + inputs.productPrice);
   };
   return (
     <>
@@ -16,19 +22,23 @@ const AddProduct = () => {
         <label>Product Name</label>
         <input
           type="text"
-          value={productName}
+          name="productName"
+          value={inputs.productName}
           placeholder="Product Name"
-          onChange={(event) => setProductName(event.target.value)}
+          onChange={handleChange}
         />
         <label>Product Price</label>
         <input
           type="number"
-          value={productPrice}
+          name="productPrice"
+          value={inputs.productPrice}
           placeholder="Product Price"
-          onChange={(event) => setProductPrice(event.target.value)}
+          onChange={handleChange}
         />
         <button type="submit">Add New Product</button>
       </form>
+      <p>Product Name: {inputs.productName}</p>
+      <p>Product Price: {inputs.productPrice}</p>
     </>
   );
 };
